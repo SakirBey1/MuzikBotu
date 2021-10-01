e2bcb49580f7fa9d42c10270d84809d1b1fc2f5b
SakirBey1 <sakirhack81@gmail.com>
2021-10-01 22:47:14 +0300

    Add files via upload

---
+++ DaisyXMusic/modules/msg.py
@@ -0,0 +1,131 @@
+# Daisyxmusic (Telegram bot project )
+# Copyright (C) 2021  Inukaasith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+from DaisyXMusic.config import SOURCE_CODE
+from DaisyXMusic.config import ASSISTANT_NAME
+from DaisyXMusic.config import PROJECT_NAME
+from DaisyXMusic.config import SUPPORT_GROUP
+from DaisyXMusic.config import UPDATES_CHANNEL
+class Messages():
+      START_MSG = "**Merhaba 👋 [{}](tg://user?id={})!**\n\n🤖 Ben telegram gruplarına Sesli sohbetlerde Müzik calmak icin yapıldım & Hoşgeldin\n\n✅ Bana  /help yazarak bilgi alabirsin."
+      HELP_MSG = [
+        ".",
+f"""
+**Hey ♪ tekrar Hoş Geldiniz {PROJECT_NAME}
+
+⚪️ {PROJECT_NAME} grubunuzun sesli sohbetinde ve kanal sesli sohbetlerinde müzik çalabilir
+
+⚪️ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions**
+""",
+
+f"""
+**Ayarlama**
+
+1) bot admin olun (cplay kullanıyorsanız grup ve kanalda)
+2) sesli sohbet başlatın
+3) bir yönetici tarafından ilk kez [şarkı adı] çalmayı /çalmayı deneyin
+* ) Userbot katıldıysa, müzik keyfini çıkarın, değilse grubunuza @{ASSISTANT_NAME} ekleyin ve tekrar deneyin
+
+** Kanal Müzik Çalma İçin**
+1) beni kanalınızın yöneticisi yapın 
+2) bağlantılı grupta / userbotjoinchannel Gönder
+3) Şimdi bağlantılı grupta komutlar gönderin
+""",
+f"""
+**Commands**
+
+**=>> Song Playing 🎧**
+
+- /oynat: Müzüği başlatir
+- /oynat: [yt url] : Belirlediginiz Yt url atarsanız calısır
+- /oynat: [reply yo audio]: her hangi bi muzugi aratabilirsin
+- /dplay: Deezerden Müzik acabilirsiniz
+- /splay: sp Platformdan Müzüğinizi baslatir
+- /ytplay: Youtubeden Müzik çalar
+
+**=>> Playback ⏯**
+
+- /player: Müzük Menüsü Açılır
+- /skip: Müzüği atlarsınız
+- /pause: Müzik durur
+- /resume: Müzik devam eder
+- /end: Durur medya lı müzik
+- /current: Geçerli çalma parçasını gösterir
+- /playlist: Çalma listesini gösterir
+
+*Player cmd ve /play, /current ve /playlist dışındaki diğer tüm cmd'ler yalnızca grubun yöneticileri içindir.
+""",
+
+f"""
+**=>> Channel Music Play 🛠**
+
+✔ Sadece bağlı grup yöneticileri için:
+
+- /cplay [şarkı adı] - istediğiniz şarkıyı çalın
+- /cdplay [şarkı adı] - deezer aracılığıyla istediğiniz şarkıyı çalın
+- /csplay [şarkı adı] - jio saavn aracılığıyla istediğiniz şarkıyı çalın
+- /cplaylist-şimdi çalma listesini göster
+- /cccurrent-şimdi göster oynuyor
+- /cplayer-açık müzik çalar ayarları paneli
+- /cpause-şarkı çalmayı Duraklat
+- /cresume-şarkı çalmaya devam et
+- /cskıp-bir sonraki şarkıyı çal
+- /cend-müzik çalmayı Durdur
+- /userbotjoinchannel-asistanınızı sohbetinize davet edin
+
+kanal c yerine de kullanılabilir (/cplay = / channelplay )
+
+⚪️ Eğer böyle oynamaya donlt varsa bağlı grup içinde :
+
+1) kanal kimliğinizi alın.
+2) tittle ile bir grup oluşturun: Kanal müzik: your_channel_id
+3) tam perma ile kanal yöneticisi olarak bot ekleyin
+4) kanala yönetici olarak @{ASSISTANT_NAME} ekleyin.
+5) sadece grubunuza komutlar gönderin. (bunun yerine /ytplay /play kullanmayı unutmayın)
+""",
+
+f"""
+**=>> Daha fazla araç 🧑🔧**
+
+- /musicplayer [on / off]: müzik çaları Etkinleştir / devre dışı bırak
+- /admincache: grubunuzun yönetici bilgilerini günceller. Bot yönetici tarafından tanınmıyorsa deneyin
+- /userbotjoin: @{ASSISTANT_NAME} kullanıcısını sohbetinize davet edin
+""",
+f"""
+**=>> Şarkı İndir 🎸**
+
+- /video [şarkı mame]: youtube'dan Video şarkı indir
+- /song [şarkı adı]: youtube'dan ses şarkısı indir
+- /saavn [şarkı adı]: saavn'dan şarkı indir
+- /deezer [şarkı adı]: deeze'den şarkı indir
+
+**=>> Arama Araçları 📄**
+
+- /search [şarkı adı]: şarkılar için YouTube'da arama yapın
+- /lyrics sözleri [şarkı adı]: Şarkı Sözleri alın
+""",
+
+f"""
+**=>> Sudo kullanıcıları için komutlar:**
+
+ - /userbotleaveall-Asistanı tüm sohbetlerden Kaldır
+ - /broadcast <Mesaja cevap ver> - global olarak brodcast tüm sohbetlere mesaj yanıtladı
+ - /pmpermit [açık/kapalı] - pmpermit mesajını Etkinleştir/devre dışı bırak
+*Sudo kullanıcıları herhangi bir gruptaki herhangi bir komutu çalıştırabilir
+
+"""
+      ]
--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# MuzikBotu
+<h1 align="centre">LoungeMUSIC V1.0 🎵</h1>
+
+### Telegram gruplarınızda müzik dinlemenize yarar 
+#### POWERED BY [Bodrumlu](https://t.me/bodrumlubebekk)
+### Destek kanalı [@Loungemusicsupport](https://t.me/loungemusicsupport)
+
+<p align="center">
+  <img src="https://telegra.ph/file/03f1c9ec4513c3a52bc22.jpg">
+</p>
+
+<h2> Özellikler 🔥 </h2>
+
+- Küçük Resim Desteği
+- Çalma Listesi Desteği
+- Mevcut oynatma desteği
+- Atlarken parça adlarını gösteriliyor
+- Sıfır kesinti, tamamen kararlı
+- Deezer, Youtube ve Saavn oynatma desteği
+- Ayarlar paneli
+- Düğmeleri ile kontrol
+- Userbot otomatik katıl
+- Kanal Müzik Çalma
+- Youtube play için klavye seçimi desteği
+
+## 🚀 Deployment
+
+### 💜 Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/bodrumlubebek/LoungeMusic)
+
+Get pyrogram (p)  `SESSION` from here:
+
+[SESSION ALMAK ICIN TIKLA](https://t.me/StringSessionGenRobot)
+
+
+### Grup için komutlar :
+#### Gruptaki herkes için
+
+- '/oynat < şarkı adı> ' - istediğiniz şarkıyı çalın
+- `/oynat <sese cevap ver> ' - yanıtlanan dosyayı oynat
+- '/dplay < şarkı adı> ' - deezer üzerinden istediğiniz şarkıyı çalın
+- '/splay <şarkı adı> ' - jio saavn aracılığıyla istediğiniz şarkıyı çalın
+- '/ytplay < şarkı adı>': doğrudan YouTube müzik üzerinden şarkı çalmak
+- '/playlist ' - şimdi çalma listesini göster
+- '/current ' - şimdi oynatmayı göster
+- '/song < song name> ' - istediğiniz şarkıları hızlı bir şekilde indirin
+- `/search <query> ' - youtube'daki videoları ayrıntılarla arayın
+- '/deezer < şarkı adı> ' - deezer üzerinden hızlı bir şekilde istediğiniz şarkıları indirin
+- '/saavn < şarkı adı> ' - saavn üzerinden hızlı bir şekilde istediğiniz şarkıları indirin
+- '/video < şarkı adı> ' - istediğiniz videoları hızlı bir şekilde indirin
+
+#### Sadece yöneticiler.
+- `/player ' - açık müzik çalar ayarları paneli
+- `/pause ' - şarkı çalmayı Duraklat
+- `/resume ' - şarkı çalmaya devam et
+- '/atla ' - bir sonraki şarkıyı çal
+- '/end ' - müzik çalmayı Durdur
+- `/userbotjoin ' - asistanınızı sohbetinize davet edin
+- `/userbotleave ' - Asistanı sohbetinizden kaldırın
+### Krediler
+
+
+#### Özel Krediler
+- [BODRUMLU OWNER](http://github.com/bodrumlu48): LoungeMusic Geliştirici
+
+
+#### DESTEK VERENLER
+- [SİRİDEV](https://t.me/sirisupport)
